c815d9c7f03d43523a86ef9c2c1d3cf8c56bd5c9
Apprentice <gin3002@gmail.com>
2015-08-13 15:02:54 +1200

    add documentation to read me (and switch info in session/user controller)

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,5 @@
-post "/session" do
-  user = User.find_by_email(params[:email])
-
-  if user.password == params[:password]
-    session[:user_id] = user.id
-    redirect "/highscore"
-  else
-    redirect "/" # Is this the login?????
-  end
-end
-
-delete "/session" do
-  session[:user_id] = nil
-
-  redirect "/"
+post "/users" do
+  user = User.create params[:user]
+  sessions[:user_id] = user.user_id
+  redirect "/highscore" # Play/help/highscore page
 end
