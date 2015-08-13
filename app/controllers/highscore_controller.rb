2f9701f2ba5dd1b90863178d9f33ea66b9b0ae48
Denis Tyurkov <dtyurkov@gmail.com>
2015-08-14 09:58:18 +1200

    ADD modified highsore controller

--- app/controllers/highscore_controller.rb
+++ app/controllers/highscore_controller.rb
@@ -29,11 +29,48 @@ get "/score" do
 
   ranked_array = Score.all.order("score DESC")
   your_ranking = ranked_array.find_index(Score.find_by_id(user.id))
-  your_ranking.to_json
+  # your_ranking.to_json
 
   score = {top: global, latest_score: latest_score, high_score: users_highest_score, rank: your_ranking}
   score.to_json
 end
 
 
+get '/highscores/global' do
+  global = {}
+  top_scores = Score.all.order('score DESC').first(10)
+  top_scores.each do |item|
+    key = User.find_by(id: item.user_id).name
+    val = item.score
+    global[key] = val
+  end
+  score_top_ten = {global: global}
+  score_top_ten.to_json
+end
 
+
+get '/highscores/user' do
+  # user = User.find(session[:user_id]) #FOR ACTUAL PURPOSES
+  user = User.find(4) #FOR TESTING PURPOSES ONLY
+  users_highest_score = user.scores.order("score DESC").first(10).to_json
+  user_high = {user_high: users_highest_score}
+  user_high.to_json
+end
+
+
+get '/highscores/user/rank' do
+  # user = User.find(session[:user_id]) #FOR ACTUAL PURPOSES
+  user = User.find(4) #FOR TESTING PURPOSES ONLY
+  ranked_array = Score.all.order("score DESC")
+  your_ranking = ranked_array.find_index(Score.find_by_id(user.id)) # Maybe + 1, as the top player potentially will be ranked 0 due to index in array.
+  user_rank = {user_rank: your_ranking}
+  user_rank.to_json
+end
+
+get '/highscores/user/latest' do
+    # user = User.find(session[:user_id]) #FOR ACTUAL PURPOSES
+  user = User.find(4) #FOR TESTING PURPOSES ONLY
+  latest_score = user.scores.last.to_json
+  user_latest = {user_latest: latest_score}
+  user_latest.to_json
+end
