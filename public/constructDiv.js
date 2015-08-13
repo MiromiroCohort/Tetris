349d671f61b01590d68eb744281947100b2308a9
Del Stevens <del.stevens@gmail.com>
2015-08-13 17:44:02 +1200

    divs set up for game board

--- public/constructDiv.js
+++ public/constructDiv.js
@@ -1,13 +1,13 @@
 function temp() {
 	var thisString = ""
 	for (var i=24; i>0; i--){
-		thisString += "<div id=\"row-"+i+"\">"
-		for (var j=1; j<11; i++) {
-			thisString += "<div id=\"row-" + i + "-cell-"+j + "\">&nbsp;</div"
+		thisString += "<div class=\"row\" id=\"row-"+i+"\">"
+		for (var j=1; j<11; j++) {
+			thisString += "<div class=\"cell\" id=\"row-" + i + "-cell-"+j + "\">&nbsp;</div>"
 		}
 		thisString += "</div>"
 	}
-	return thisString
+	console.log(thisString)
 }
 
 temp()
