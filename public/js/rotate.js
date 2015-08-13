9916fe1e7e7dc08d9635ed3da4819a2924f6a4b2
IonaCS <apprentice@StMac8.local>
2015-08-14 11:24:00 +1200

    game mechanics is working

--- public/js/rotate.js
+++ public/js/rotate.js
@@ -1,28 +1,40 @@
+
+function emptySquareArray(length)
+{ var fliped=[]
+for(var i=0;i<length;i++)
+    {  temp= [] ;
+
+      for(var j=0;j < length;j++)
+      {
+          temp.push(0);
+      }
+      // console.log temp
+      fliped.push(temp);
+    }
+    return fliped
+  }
+
 function rotateLeft(array)
 {
-  // var fliped=[]
-
-//   for(var i=0;i<array.length;i++)
-//     {  temp= [] ;
-
-//       for(var j=0;j < array[i].length;j++)
-//       {
-//           temp << 0;
-//       }
-//       fliped << temp;
-//     }
-var fliped=[[0,0,0],[0,0,0],[0,0,0]]
-
+  var fliped=emptySquareArray(array.length)
   for(var i=0;i<array.length;i++)
    {
       for(var j=0;j < array[i].length;j++)
       {
         fliped[j][array[i].length-1-i] = array[i][j];
-        // console.log("i=");
-        // console.log(i);
-        // console.log("j=");
-        // console.log(j);
+      }
+   }
 
+   return fliped
+}
+function rotateRight(array)
+{
+  var fliped=emptySquareArray(array.length)
+  for(var i=0;i<array.length;i++)
+   {
+      for(var j=0;j < array[i].length;j++)
+      {
+        fliped[array[i].length-j-1][i] = array[i][j];
       }
    }
 
@@ -31,4 +43,10 @@ var fliped=[[0,0,0],[0,0,0],[0,0,0]]
 var a=[[1,0,0],[1,0,0],[1,1,0]]
 console.log(a);
 a=rotateLeft(a);
+console.log(a);
+a=rotateLeft(a);
+console.log(a);
+a=rotateRight(a);
+console.log(a);
+a=rotateRight(a);
 console.log(a);
