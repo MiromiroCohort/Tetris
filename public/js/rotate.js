fc80149b737fb07382bb6133b7d623b86a03416c
IonaCS <apprentice@StMac8.local>
2015-08-14 15:39:13 +1200

    moving right is working

--- public/js/rotate.js
+++ public/js/rotate.js
@@ -1,11 +1,11 @@
 
 function emptySquareArray(length)
 {
-  var fliped=[]
-  for(var i=0;i<length;i++)
-      {  temp= [] ;
+  var fliped = []
+  for(var i =0;i<length;i++)
+      {  temp = [] ;
 
-        for(var j=0;j < length;j++)
+        for(var j = 0;j < length;j++)
         {
             temp.push(0);
         }
@@ -18,9 +18,9 @@ function emptySquareArray(length)
 function rotateLeft(array)
 {
   var fliped=emptySquareArray(array.length)
-  for(var i=0;i<array.length;i++)
+  for(var i = 0;i<array.length;i++)
    {
-      for(var j=0;j < array[i].length;j++)
+      for(var j = 0;j < array[i].length;j++)
       {
         fliped[j][array[i].length-1-i] = array[i][j];
       }
@@ -30,10 +30,10 @@ function rotateLeft(array)
 
 function rotateRight(array)
 {
-  var fliped=emptySquareArray(array.length)
-  for(var i=0;i<array.length;i++)
+  var fliped = emptySquareArray(array.length)
+  for(var i = 0;i < array.length;i++)
    {
-      for(var j=0;j < array[i].length;j++)
+      for(var j = 0;j < array[i].length;j++)
       {
         fliped[array[i].length-j-1][i] = array[i][j];
       }
@@ -43,10 +43,10 @@ function rotateRight(array)
 
 function game_board()
 {
-  var new_board=[]
-  for(var i=0;i<24;i++)
-  { temp= [] ;
-    for(var j=0;j<10;j++)
+  var new_board = []
+  for(var i = 0;i<24;i++)
+  { temp = [] ;
+    for(var j = 0;j<10;j++)
     {
       temp.push(0);
     }
@@ -59,9 +59,9 @@ function inject_figure(board,figure,left_corner)
 {
     for(var i = left_corner.i; i<left_corner.i+figure.length;i++)
     {
-      for(var j=left_corner.j; j<left_corner.j+figure.length;j++)
+      for(var j = left_corner.j; j<left_corner.j+figure.length;j++)
       {
-        board[i][j]=figure[i-left_corner.i][j-left_corner.j]
+        board[i][j] = figure[i-left_corner.i][j-left_corner.j]
       }
     }
 }
@@ -75,7 +75,7 @@ function isMovePossible(board,figure,left_corner)
      }
      for(j=left_corner.j;j<left_corner.j+figure.length;j++)
      {
-       if ((board[i][j] == 1)& (board[i-1][j] == 1)) return false;
+       if ((board[i][j] > 0)& (board[i-1][j] > 0)) return false;
      }
      return true;
 }
@@ -102,68 +102,148 @@ function isMoveRightDownPossible(board,figure,left_corner)
      }
      for(var j = left_corner.j+1; j<left_corner.j+figure.length+1; j++)
      {
-       if ((board[i][j] == 1)& (board[i-1][j-1] == 1)) return false;
+       if ((board[i][j] > 0)& (board[i-1][j-1] > 0)) return false;
      }
-     var j = left_corner+figure.length
-     if (j == 10) return false;
+     var j = left_corner.j+figure.length;
+
+      if (j == 10) return false;
      for(count = left_corner.i+1; count < left_corner.i+figure.length+1; count++)
      {
-       if ((board[count][j] == 1)& (board[count-1][j-1] == 1)) return false;
+       if ((board[count][j] > 0)& (board[count-1][j-1] > 0)) return false;
      }
      return true;
+}
+function isMoveLeftDownPossible(board,figure,left_corner)
+{
+   var i=left_corner.i+figure.length;
+     if (i == 24)
+     {
+      return false;
+     }
+     for(var j = left_corner.j+1; j<left_corner.j+figure.length+1; j++)
+     {
+       if ((board[i][j] > 0)& (board[i-1][j+1] > 0)) return false;
+     }
+     var j = left_corner.j-1;
+
+      if (j == 0) return false;
+     for(count = left_corner.i+1; count < left_corner.i+figure.length+1; count++)
+     {
+       if ((board[count][j] > 0)& (board[count-1][j+1] > 0)) return false;
+     }
+     return true;
+}
+function isFigurePositionedRight(figure)
+{ var j=figure.length-1
+   for(var i = 0; i < figure.length; i++)
+   {
+     if (figure[i][j] > 0 ) return true;
+   }
+   return false;
+}
+function positionFigureRight(figure)
+{
+   for (var i=0; i < figure.length; i++)
+   {
+     for (var j=figure.length-1;j>0; j--)
+     {
+        figure[i][j]=figure[i][j-1];
+     }
+        figure[i][0]=0;
+   }
+}
+function isFigurePositionedLeft(figure)
+{ var j=0;
+   for(var i = 0; i < figure.length; i++)
+   {
+     if (figure[i][j] > 0 ) return true;
+   }
+   return false;
+}
+
+function positionFigureLeft(figure)
+{
+   for (var i=0; i < figure.length; i++)
+   {
+     for (var j=0;j<figure.length-1; j++)
+     {
+        figure[i][j]=figure[i][j+1];
+     }
+        figure[i][figure.length-1]=0;
+   }
 }
 
 function moveFigureRightDown(board,figure,left_corner)
 {
-  for(var i = left_corner.i; i<left_corner.i+figure.length;i++)
+  for(var i = left_corner.i; i < left_corner.i+figure.length;i++)
     {
-      for(var j=left_corner.j; j<left_corner.j+figure.length;j++)
+      for(var j = left_corner.j; j < left_corner.j+figure.length;j++)
       {
-        board[i][j]=0;
+        board[i][j] = 0;
       }
     }
-    left_corner.i=left_corner.i+1; left_corner.j=left_corner.j+1;
+    if(!isFigurePositionedRight(figure))
+      {
+        positionFigureRight(figure)
+      }
+    else
+    {
+      left_corner.i=left_corner.i+1; left_corner.j = left_corner.j+1;
+    }
     inject_figure(board,figure,left_corner)
-
 }
+
 function moveFigureLeftDown(board,figure,left_corner)
 {
-  for(var i = left_corner.i; i<left_corner.i+figure.length;i++)
+  for(var i = left_corner.i; i < left_corner.i+figure.length; i++)
     {
-      for(var j=left_corner.j; j<left_corner.j+figure.length;j++)
+      for(var j = left_corner.j; j < left_corner.j+figure.length; j++)
       {
-        board[i][j]=0;
+        board[i][j] = 0;
       }
     }
-    left_corner.i=left_corner.i+1; left_corner.j=left_corner.j-1
+    if(!isFigurePositionedLeft(board,figure,left_corner))
+    {
+      positionFigureLeft(figure);
+    }
+    else
+    {
+      left_corner.i=left_corner.i+1; left_corner.j=left_corner.j-1
+    }
     inject_figure(board,figure,left_corner);
 
 }
 
 
-var a=[[1,0,0],[1,0,0],[1,1,0]];
-board=game_board();
-var left_corner={i:4-a.length,j:5}
+var a = [[1,0,0],[1,0,0],[1,1,0]];
+board = game_board();
+var left_corner = {i:4-a.length,j:5}
 inject_figure(board, a, left_corner);
 console.log(board);
 while(isMoveRightDownPossible(board,a,left_corner))
 {
 moveFigureRightDown(board,a,left_corner)
+console.log(board);
+}
+while(isMoveLeftDownPossible(board,a,left_corner))
+{
+  moveFigureLeftDown(board,a, left_corner)
+  console.log(board);
 }
 
-// while (isMovePossible(board,a,left_corner))
-// {
-//   moveFigureDown(board,a,left_corner)
-//   console.log(board);
-// }
-// b =[[1,0,0],[1,0,0],[1,1,0]];
-// left_corner={i:4-a.length,j:5}
-// inject_figure(board, b, left_corner);
-// while (isMovePossible(board,b,left_corner))
-// {
-//   moveFigureDown(board,b,left_corner)
-//   console.log(board);
-// }
+while (isMovePossible(board,a,left_corner))
+{
+  moveFigureDown(board,a,left_corner)
+  console.log(board);
+}
+b =[[1,0,0],[1,0,0],[1,1,0]];
+left_corner={i:4-a.length,j:5}
+inject_figure(board, b, left_corner);
+while (isMovePossible(board,b,left_corner))
+{
+  moveFigureDown(board,b,left_corner)
+  console.log(board);
+}
 // c=[[1,1],[1,1]]
 // left_corner={i:4-c.length,j:5}
 // inject_figure(board, c, left_corner);
